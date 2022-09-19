bb4ce76fed3ca48a849e81a2c7715ee69695041f
Rian Dermawan <109790897+RianDermawan@users.noreply.github.com>
2022-09-19 11:39:00 +0700

    Create Biodata with python.py

---
+++ Biodata with python.py
@@ -0,0 +1,2 @@
+# Profile
+print('Full Name\t\t: Rian Dermawan\nSex\t\t\t: Male\nPlace & Date of Birth\t: Garut, 7 February 2000\nPhone Number\t\t: 085795025575\nE-mail\t\t\t: riandermawan.work@gmail.com\nCitizenship\t\t: Indonesia\nReligion\t\t: Muslim\nBlood Type\t\t: A\nHobbies\t\t\t: Explore new things\nSocial Media\t\t: www.linkedin.com/in/riandermawan')
